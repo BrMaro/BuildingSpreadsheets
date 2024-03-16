4c9c64c7e59a2983b1fc837c3287d9761e94f7d2
BrMaro <70138725+BrMaro@users.noreply.github.com>
2024-03-16 12:47:06 +0300

    median function

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ const sum = (nums) => nums.reduce((acc,el)=>acc+el);
 
 const isEven = (num) => num%2==0 ? true : false;
 
+const median = (nums) => {
+  const sorted = nums.slice().sort((a,b)=>a-b);
+  const length = sorted.length;
+  const middle = length/2-1;
+  return isEven(length) ? average([sorted[middle],sorted[middle+1]]) : sorted[Math.ceil(middle)]
+}
+
 const average = (nums) => sum(nums)/nums.length;
 
 const range = (start,end) => Array(end-start+1).fill(start).map( (element,index) => element+index )
